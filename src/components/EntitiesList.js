4347c16b2f0f2dfe86eb0f0dab42e713e025e6a4
TanEngLiatJermin <jermint87@gmail.com>
2024-12-26 13:35:11 +0800

    updated entities list with delete btn

--- src/components/EntitiesList.js
+++ src/components/EntitiesList.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const EntitiesList = ({ entities }) => {
+const EntitiesList = ({ entities, onDeleteEntity }) => {
   return (
     <ul>
       {entities.map((entity) => (
         <li key={entity.id} data-testid="EntityTestId">
-          <span data-testid="EntityCategoryTestId">{entity.category}</span> -{" "}
-          {entity.name}
+          <span data-testid="EntityCategoryTestId">{entity.category}</span> - {entity.name}
+          <button
+            onClick={() => onDeleteEntity(entity.id)}
+            data-testid="EntityDeleteTestId"
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
